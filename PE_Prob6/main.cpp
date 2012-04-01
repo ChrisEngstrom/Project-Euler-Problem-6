dbc1c955c284c86c6ad491fc32ee0f58d1e9c9c9
Chris Engstrom <tipher88@gmail.com>
2012-04-01 11:47:01 -0500

    Initial commit.

---
+++ PE_Prob6/main.cpp
@@ -0,0 +1,18 @@
+//****************************
+// Chris Engstrom            *
+// Project Euler - Problem 6 *
+//*******************************************************************
+// Find the difference between the sum of the squares of the first  *
+//  one hundred natural numbers and the square of the sum.          *
+//*******************************************************************
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+
+
+    return 0;
+}
