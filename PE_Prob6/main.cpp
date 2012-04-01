65631dc2cd90d434e53267a0811200181f82eace
Chris Engstrom <tipher88@gmail.com>
2012-04-01 12:37:07 -0500

    Solved the problem.

--- PE_Prob6/main.cpp
+++ PE_Prob6/main.cpp
@@ -12,7 +12,24 @@ using namespace std;
 
 int main() {
 
+    int maxNum = 100,
+        sumOfSquares = 0,
+        squareOfSum = 0;
 
+    // Find the sum of squares
+    for(int i = 1; i < (maxNum + 1); i++)
+    {
+        // Hold the sum of the numbers to use later
+        squareOfSum += i;
+
+        // Calculate the sum of the squares as we iterate
+        sumOfSquares += (i *i);
+    }
+
+    // Find the square of the sum
+    squareOfSum = (squareOfSum * squareOfSum);
+
+    cout << "The difference between the sum of the squares and the square of the sum is: " << endl << squareOfSum - sumOfSquares << endl << endl;
 
     return 0;
 }
